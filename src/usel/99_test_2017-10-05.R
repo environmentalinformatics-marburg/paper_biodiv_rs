43fda60c33b9725d34aa827751db2cd81721930c
tnauss <nauss@staff.uni-marburg.de>
2017-10-05 22:05:36 +0200

    do some testing

--- src/usel/99_test_2017-10-05.R
+++ src/usel/99_test_2017-10-05.R
@@ -1,83 +1,22 @@
-
+# Some testing
 # Set path ---------------------------------------------------------------------
 source("F:/exploratorien/scripts/00_set_environment.R")
-veg_re_g = readRDS(paste0(path_rdata, "uselig_initial_model.rds"))
+source("C:/Users/tnauss/permanent/plygrnd/exploratorien/project_biodiv_rs/src/tnauss/00_set_environment.R")
+library(doParallel)
+library(ggplot2)
+library(foreach)
+library(reshape2)
 
 
-compute = TRUE
-
-# Compile grassland diversity and remote sensing -------------------------------
-if(compute){
-  vegrel14 <- readRDS(file = paste0(path_rdata, "vegrel14.rds"))
+# Start with model without split mult resp:
+veg_re_g_gpm = readRDS(paste0(path_rdata, "uselig_initial_model.rds"))
+belc = c("AEG", "HEG", "SEG")
+veg_re_g_gpm_indv = lapply(belc, function(be){
+  return(splitMultRespLSO(x = veg_re_g_gpm[[be]], nbr = 5))
   
-  vegrel15 <- readRDS(file = paste0(path_rdata, "vegrel15.rds"))
-  
-  meta <- readRDS(paste0(path_rdata, "meta.rds"))
-  
-  re <- readRDS(paste0(path_rdata, "re_predictors.rds"))
-  re_df <- lapply(re, function(e){
-    as.data.frame(e)
-  })
-  re_df <- do.call("rbind", re_df)
-  re_df$Year <- 2015
-  nas <- colSums(is.na(re_df))
-  re_df <- re_df[, which(nas < 1)]
-  
-  veg_re_g <- merge(vegrel15, re_df, by.x = c("EPID", "Year"),
-                    by.y = c("EP", "Year"))
-  max(colSums(is.na(veg_re_g)))
-  
-  veg_re_g <- merge(veg_re_g, meta, by.x = c("EPID", "Year"),
-                    by.y = c("plotID", "datetime"))
-  
-  
-  saveRDS(veg_re_g, paste0(path_rdata, "veg_re_g.rds"))
-} else {
-  veg_re_g <- readRDS(paste0(path_rdata, "veg_re_g.rds"))
-}
-
-# Prepare gpm data set used for remote sensing prediction study ----------------
-belc <- c("AEG", "HEG", "SEG")
-veg_re_g_gpm_indv <- lapply(belc, function(b){
-  act_veg_re_g <- veg_re_g[veg_re_g$g_belc == b, ]
-  
-  col_selector <- which(names(act_veg_re_g) == "EPID")
-  
-  col_diversity <- c(grep("specrich|shannon|eveness", 
-                          tolower(names(act_veg_re_g))),
-                     seq(grep("vegetation_height_mean_cm",
-                              names(act_veg_re_g)),
-                         grep("biomass_g",
-                              names(act_veg_re_g))),
-                     seq(grep("G_std_glb",
-                              names(act_veg_re_g)),
-                         grep("LUI_reg",
-                              names(act_veg_re_g))))
-  
-  col_precitors <- seq(grep("GLI_mean", names(act_veg_re_g)), 
-                       grep("pca_Long_Run_Low_Grey.Level_Emphasis.b1r50o1_var", names(act_veg_re_g)))
-  
-  col_precitors <- col_precitors[-grep("ep|id|type", tolower(names(act_veg_re_g[, col_precitors])))]
-  
-  col_meta <- seq(length(act_veg_re_g))[-c(col_selector, col_diversity, col_precitors)]
-  
-  meta <- createGPMMeta(act_veg_re_g, type = "input",
-                        selector = col_selector,
-                        response = col_diversity,
-                        predictor = col_precitors,
-                        meta = col_meta)
-  veg_re_g_gpm <- gpm(act_veg_re_g, meta, scale = FALSE)
-  
-  # Clean predictor variables 
-  veg_re_g_gpm <- cleanPredictors(x = veg_re_g_gpm, nzv = TRUE,
-                                  highcor = TRUE, cutoff = 0.80)
-  
-  # Compute resamples following a leave location out approach
-  veg_re_g_gpm <- splitMultRespLSO(x = veg_re_g_gpm, nbr = 1)
 })
-names(veg_re_g_gpm_indv) <- belc
-# save the object with 100 predictors and make training
-saveRDS(veg_re_g_gpm_indv, file = paste0(path_rdata, "veg_rs_100.rds"))
+names(veg_re_g_gpm_indv) = belc
+veg_re_g_gpm_indv[["AEG"]]@meta$input$TRAIN_TEST
 
 #only use predictors appearing in all 3 exploratories (leaves us eventually with 24 predictors)
 predictors_common <- veg_re_g_gpm_indv[[1]]@meta$input$PREDICTOR_FINAL[veg_re_g_gpm_indv[[1]]@meta$input$PREDICTOR_FINAL %in% veg_re_g_gpm_indv[[2]]@meta$input$PREDICTOR_FINAL]
@@ -90,8 +29,106 @@ veg_re_g_gpm_indv[[1]]@meta$input$PREDICTOR_FINAL <- predictors_common
 veg_re_g_gpm_indv[[2]]@meta$input$PREDICTOR_FINAL <- predictors_common
 veg_re_g_gpm_indv[[3]]@meta$input$PREDICTOR_FINAL <- predictors_common
 
-saveRDS(veg_re_g_gpm_indv, file = paste0(path_rdata, "gpm_obj_24pred.rds"))
+# Test: just use some of them...
+for(i in seq(3)){
+  veg_re_g_gpm_indv[[i]]@meta$input$PREDICTOR_FINAL = c("PC3_median", "PC3_sd", "PC1_sd",
+                                                        "PC1_median", "PC2_sd", "PC5_median")
+}
 
-#####  test with 3 predictors
-#preds3<-c(veg_re_g_gpm_indv[[1]]@meta$input$PREDICTOR_FINAL[-c(4:24)])
-#veg_re_g_gpm_indv[[1]]@meta$input$PREDICTOR_FINAL<-preds3
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+
+test_model <- trainModel(x = veg_re_g_gpm_indv[["AEG"]],
+                         n_var = NULL,             
+                         mthd = "pls", #rf, gam, glmboost
+                         mode = "ffs",
+                         seed_nbr = 11, 
+                         cv_nbr = 9,
+                         var_selection = "indv",
+                         response_nbr = 2,
+                         filepath_tmp = path_temp)
+
+# saveRDS(test_model, file = paste0(path_rdata, "test_model_2017-10-05.rds"))
+# test_model = readRDS(file = paste0(path_rdata, "test_model_2017-10-05.rds"))
+
+tstat = compRegrTests(models = test_model@model[[1]], per_model = TRUE, 
+                      per_selector = FALSE, sub_selectors = NULL, 
+                      details = TRUE)
+
+tstat_lm_smpl_r2 <- lapply(unique(tstat$sample), function(s){
+  dt = tstat[tstat$sample == s, ]
+  tstat_lm_smpl = lm(testing_predicted ~ testing_response, data = dt)
+  data.frame(response = dt$model_response[1],
+             tstat_lm_smpl_r2 = summary(tstat_lm_smpl)$r.squared,
+             smpl = dt$sample[1])
+})
+tstat_lm_smpl_r2 = do.call("rbind", tstat_lm_smpl_r2)
+tstat_errors = melt(tstat_lm_smpl_r2,  id.vars = c("response", "smpl"))
+tstat_errors$variable = "r_squared"
+
+tstat_lm_all = lm(testing_predicted ~ testing_response, data = tstat)
+tstat_lm_all_r2 = data.frame(response = tstat$model_response[1],
+                             smpl = "all",
+                             variable = "r_squared",
+                             value = summary(tstat_lm_all)$r.squared)
+tstat_errors = rbind(tstat_errors, tstat_lm_all_r2)
+
+tstat_lm_set_r2 = data.frame(response = tstat$model_response[1],
+                             smpl = "set", 
+                             variable = "r_squared",
+                             value = mean(tstat$r_squared))
+tstat_errors = rbind(tstat_errors, tstat_lm_set_r2)
+
+tstat_smpl_rmse <- lapply(unique(tstat$sample), function(s){
+  dt = tstat[tstat$sample == s, ]
+  data.frame(response = tstat$model_response[1],
+             tstat_smpl_rmse = sqrt(mean((dt$testing_predicted - dt$testing_response)**2)),
+             smpl = dt$sample[1])
+})
+tstat_smpl_rmse = do.call("rbind", tstat_smpl_rmse)
+tstat_smpl_rmse = melt(tstat_smpl_rmse,  id.vars = c("response", "smpl"))
+tstat_smpl_rmse$variable = "rmse"
+tstat_errors = rbind(tstat_errors, tstat_smpl_rmse)
+
+tstat_rmse_all = data.frame(response = tstat$model_response[1],
+                            smpl = "all",
+                            variable = "rmse",
+                            value = sqrt(mean((tstat$testing_predicted - tstat$testing_response)**2)))
+tstat_errors = rbind(tstat_errors, tstat_rmse_all)
+tstat_errors$stat = "test"
+tstat_errors$be = substr(tstat$model_selector, 1, 3)[1]
+
+
+mstat = lapply(test_model@model$pls_ffs[[1]], function(s){
+  data.frame(be = substr(s$training$SELECTOR[1], 1, 3),
+             response = s$response,
+             r_squared = max(s$model$results$Rsquared),
+             r_squared_sd = max(s$model$results$RsquaredSD),
+             rmse = max(s$model$results$RMSE),
+             rmse_sd = max(s$model$results$RMSESD))
+})
+mstat = do.call("rbind", mstat)
+mstat$smpl = seq(10)
+mstat = melt(mstat, id.var = c("response", "be", "smpl"))
+mstat$stat = "train"
+
+
+errors = rbind(tstat_errors, mstat)
+unique(errors$variable)
+
+ggplot(data = errors[errors$smpl %in% seq(10) & errors$variable == "rmse",], 
+       aes(x = response, y = value, color = stat)) + 
+  geom_boxplot() + 
+  geom_point(data = errors[errors$smpl == "all" & errors$variable == "rmse",],
+             aes(x = response, y = value, color="RMSE all")) + 
+  theme_bw()
+
+
+ggplot(data = errors[errors$smpl %in% seq(10) & errors$variable == "r_squared",], 
+       aes(x = response, y = value, color = stat)) + 
+  geom_boxplot() + 
+  geom_point(data = errors[errors$smpl == "set" & errors$variable == "r_squared",],
+             aes(x = response, y = value, color = "Mean R2 from sets")) + 
+  geom_point(data = errors[errors$smpl == "all" & errors$variable == "r_squared",],
+             aes(x = response, y = value, color = "R2 over all")) + 
+  theme_bw()
