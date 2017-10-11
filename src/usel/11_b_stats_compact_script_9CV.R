46750e725ba642fb4f80b977af18786b1ac9d5c3
Usel <ul.selig@gmail.com>
2017-10-11 12:22:08 +0200

    add script statistic for 9CV and new calc of R-Sq. und RMSE

---
+++ src/usel/11_b_stats_compact_script_9CV.R
@@ -0,0 +1,111 @@
+
+# regressiontests
+tstat<-lapply(spec,function(be){
+  tstat <- compRegrTests(models = be@model$pls_ffs, per_model = TRUE, 
+                         per_selector = FALSE, sub_selectors = NULL, 
+                         details = TRUE)
+  return(list(tstat=tstat))
+})
+
+######## TESTING/VALIDATION DATA------------------------------------------------------------
+# we want the Rqu/RMSE from each RESAMPLE(10 iterations) set (eventually 10 values per response and Explo.)
+tstat_tstat_lm_smpl_r2 <-lapply(tstat, function(be){
+  
+  mr<-lapply(unique(be$tstat$model_response), function(mr){
+    dt_r<-be$tstat[be$tstat$model_response == mr,]
+    be_stats<- lapply(unique(be$tstat$sample), function(s){
+      dt <- be$tstat[be$tstat$sample == s & be$tstat$model_response == mr, ]
+      tstat_lm_smpl <- lm(testing_predicted ~ testing_response, 
+                          data = dt)
+      rmse<- sqrt(mean(dt$testing_predicted- dt$testing_response)**2)
+      data.frame(be = substr(be$tstat$model_selector[1], 1, 3), 
+                 response = dt$model_response[3],
+                 r_squared = summary(tstat_lm_smpl)$r.squared, #smry cause we have 5samples held out and we want 1 value per RESAMPLE
+                 rmse= rmse,
+                 smpl = dt$sample[2])
+    })
+    do.call("rbind",be_stats)  
+  })
+  do.call("rbind",mr)
+})
+tstat_lm_smpl_r2 = do.call("rbind", tstat_tstat_lm_smpl_r2)
+rownames(tstat_lm_smpl_r2) <- NULL
+
+# we cal. Rsq/RMSE as "usual" (one R-value per response) "all" or "Rsum"
+sum_r <- lapply(tstat, function(be){
+  be_stats <- lapply(unique(be$tstat$model_response), function(mr){
+    rs <- summary(lm(testing_predicted ~ testing_response,
+                     data = be$tstat[be$tstat$model_response == mr, ]))$r.squared
+    rmse <- sqrt(mean((be$tstat[be$tstat$model_response == mr, ]$testing_predicted -
+                         be$tstat[be$tstat$model_response == mr, ]$testing_response)**2))
+    data.frame(be = substr(be$tstat$model_selector[1], 1, 3),
+               response = mr, 
+               smpl = "Rsum",
+               r_squared =rs ,
+               rmse = rmse)
+  })
+  do.call("rbind", be_stats)
+})
+sum_r <- do.call("rbind", sum_r) 
+rownames(sum_r) <- NULL
+
+tstat_errors = rbind(tstat_lm_smpl_r2, sum_r)
+
+# calc. RMEAN for the RESAMPLES (10 rmean values)
+rmean <-lapply(tstat, function(be){
+  
+  mr<-lapply(unique(be$tstat$model_response), function(mr){
+    dt_r<-be$tstat[be$tstat$model_response == mr,]
+    dt <- be$tstat[be$tstat$model_response == mr, ]
+    rmean <- mean(dt$r_squared)
+    rmse <- sqrt(mean(dt$testing_predicted- dt$testing_response)^2)
+    data.frame(be = substr(be$tstat$model_selector[1], 1, 3), 
+               response = dt$model_response[3],
+               r_squared= rmean,
+               rmse= rmse,
+               smpl="Rmean")
+  })
+  do.call("rbind",mr)  
+})
+rmean = do.call("rbind", rmean)
+rownames(rmean)<-NULL
+
+tstat_errors = rbind(tstat_errors, rmean)
+
+# melt the values all in one column
+tstat_errors<-melt(tstat_errors, id.var=c("response","be","smpl"))
+tstat_errors$stat = "test"
+
+####### TRAINING data--------------------------------------------
+
+mstat <- lapply(C9V, function(be){
+  mod_r <- lapply(seq(length(be@model[[1]])), function(r){
+    mod_s <- lapply(seq(length(be@model[[1]][[r]])), function(s){
+      if(class(be@model[[1]][[r]][[s]]$model) == "try-error"){
+        df <- NULL
+      } else {
+        df <- data.frame(be = substr(be@model[[1]][[r]][[s]]$training$SELECTOR[1], 1, 3), 
+                         response = be@model[[1]][[r]][[s]]$response,
+                         r_squared = max(be@model[[1]][[r]][[s]]$model$results$Rsquared),
+                         r_squared_sd = max(be@model[[1]][[r]][[s]]$model$results$RsquaredSD),
+                         rmse = min(be@model[[1]][[r]][[s]]$model$results$RMSE),
+                         rmse_sd = min(be@model[[1]][[r]][[s]]$model$results$RMSESD))
+      }
+      return(df)
+    })
+    return(do.call("rbind", mod_s))
+  })
+  return(do.call("rbind", mod_r))
+})
+mstat <- do.call("rbind", mstat)
+rownames(mstat) <- NULL
+
+#for each resampling a ID
+mstat$smpl = seq(10)
+# make all values in a column
+mstat = melt(mstat, id.var = c("response", "be", "smpl"))
+mstat$stat = "train"
+
+# join train and test data together
+errors = rbind(tstat_errors, mstat)
+unique(errors$variable) #check if all calcs are there there
