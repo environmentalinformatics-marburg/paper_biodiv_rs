e525b954dd9c55665b02d88824a305a5f65021a8
Usel <ul.selig@gmail.com>
2017-10-26 12:08:35 +0200

    postprocessing after 9CV modeling

--- src/usel/11c_stats_compact_script_9CV.R
+++ src/usel/11c_stats_compact_script_9CV.R
@@ -1,14 +1,15 @@
-source("D:/UNI/Master/MA/exploratorien/scripts/project_biodiv_rs/src/usel/00_set_environment.R")
-spec<-readRDS(paste0(path_results,"test_model_spec_7pred.rds"))
-
+source("D:/UNI/Master/MA/exploratorien/scripts/project_biodiv_rs/src/usel/00_b_set_environment_start_modeltrain.R")
+x<-readRDS(paste0(path_results,"9CV_4models_13pred.rds"))
+gams<-readRDS(paste0(path_results,"9CV_glm_HEG_SEG.rds"))
+x$HEG@model$glmboost_ffs<-gams$HEG@model$glmboost_ffs
+x$SEG@model$glmboost_ffs<-gams$SEG@model$glmboost_ffs
+saveRDS(x,paste0(path_results,"9CV_4models_13pred.rds"))
 #tstat<-tstat$AEG
 #tstat<-tstat$tstat[tstat$tstat$response=="SPECRICH",]
 
 # regressiontests
-tstat<-lapply(spec,function(be){
-  tstat <- compRegrTests(models = be@model$pls_ffs, per_model = TRUE, 
-                         per_selector = FALSE, sub_selectors = NULL, 
-                         details = TRUE)
+tstat<-lapply(x,function(be){
+  tstat <- compRegrTests( be@model$glmboost_ffs)
   return(list(tstat=tstat))
 })
 
@@ -26,10 +27,10 @@ tstat_tstat_lm_smpl_r2 <-lapply(tstat, function(be){
       #rmse<-rmse(dt$testing_predicted, dt$testing_response)
       rmse<-RMSE(dt$testing_predicted, dt$testing_response) # doesnt matter which fun
       data.frame(be = substr(be$tstat$model_selector[1], 1, 3), 
-                 response = dt$model_response[3],
+                 response = dt$model_response[3], #auf 1 bei mehreren responses
                  r_squared = summary(tstat_lm_smpl)$r.squared, #smry cause we have 5samples held out and we want 1 value per RESAMPLE (but we dont actually calculate it, the value is already there)
                  rmse= rmse,
-                 smpl = dt$sample[2])
+                 smpl = dt$sample[2]) #auf 1 bei mehreren responses
     })
     do.call("rbind",be_stats)  
   })
@@ -65,7 +66,8 @@ rmean <-lapply(tstat, function(be){
     #dt_r<-be$tstat[be$tstat$model_response == mr,]
     dt <- be$tstat[be$tstat$model_response == mr, ]
     rmean <- mean(dt$r_squared)
-    rmse <- sqrt(mean(dt$testing_predicted- dt$testing_response)^2)
+    #rmse <- sqrt(mean(dt$testing_predicted- dt$testing_response)^2)
+    rmse<-RMSE(dt$testing_predicted, dt$testing_response)
     data.frame(be = substr(be$tstat$model_selector[1], 1, 3), 
                response = dt$model_response[3],
                r_squared= rmean,
@@ -85,18 +87,18 @@ tstat_errors$stat = "test"
 
 ####### TRAINING data--------------------------------------------
 
-mstat <- lapply(spec, function(be){
-  mod_r <- lapply(seq(length(be@model[[1]])), function(r){
-    mod_s <- lapply(seq(length(be@model[[1]][[r]])), function(s){
-      if(class(be@model[[1]][[r]][[s]]$model) == "try-error"){
+mstat <- lapply(x, function(be){
+  mod_r <- lapply(seq(length(be@model$glmboost_ffs)), function(r){
+    mod_s <- lapply(seq(length(be@model$glmboost_ffs[[r]])), function(s){
+      if(class(be@model$glmboost_ffs[[r]][[s]]$model) == "try-error"){
         df <- NULL
       } else {
-        df <- data.frame(be = substr(be@model[[1]][[r]][[s]]$training$SELECTOR[1], 1, 3), 
-                         response = be@model[[1]][[r]][[s]]$response,
-                         r_squared = be@model[[1]][[r]][[s]]$model$results$Rsquared,
-                         #r_squared_sd = be@model[[1]][[r]][[s]]$model$results$RsquaredSD,
-                         rmse = be@model[[1]][[r]][[s]]$model$results$RMSE)
-                         #rmse_sd = be@model[[1]][[r]][[s]]$model$results$RMSESD)
+        df <- data.frame(be = substr(be@model$glmboost_ffs[[r]][[s]]$training$SELECTOR[1], 1, 3), 
+                         response = be@model$glmboost_ffs[[r]][[s]]$response,
+                         r_squared = be@model$glmboost_ffs[[r]][[s]]$model$results$Rsquared,
+                         #r_squared_sd = be@model$glmboost_ffs[[r]][[s]]$model$results$RsquaredSD,
+                         rmse = be@model$glmboost_ffs[[r]][[s]]$model$results$RMSE)
+                         #rmse_sd = be@model$glmboost_ffs[[r]][[s]]$model$results$RMSESD)
       }
       return(df)
     })
@@ -133,4 +135,4 @@ mstat$stat = "train"
 errors = rbind(tstat_errors, mstat)
 unique(errors$variable) #check if all calcs are there there
 
-saveRDS(errors,paste0(path_stats,"allnewstats_9CV_SPEC"))
+saveRDS(errors,paste0(path_stats,"allnewstats_GLM_9CV_allRESP.rds"))
--- src/usel/11d_vis_9CV.R
+++ src/usel/11d_vis_9CV.R
@@ -1,7 +1,11 @@
+source("D:/UNI/Master/MA/exploratorien/scripts/project_biodiv_rs/src/usel/00_set_environment.R")
+
+stats<-readRDS(paste0(path_stats,"allnewstats_GLM_9CV_allRESP.rds"))
 # visualisation ideas
 cbPalette<-c("#ffffff", "#525252", "#bdbdbd") #bnw palette
-
-mr_all <- c("SPECRICH", "SHANNON", "EVENESS")
+cbPalette <- c("#fbb4ae", "#80b1d3", "#b3de69")
+palette<-c("#BuGn","#OrRd","#PuRd")
+mr_all <- c("SPECRICH", "SHANNON", "EVENESS", "LUI_glb")
 mr_all<-"SHANNON"
 
 # lets try it with SPEC only
@@ -17,7 +21,7 @@ ggplot(data = specerror[specerror$smpl %in% seq(10) & specerror$variable == "r_s
              aes(x = response, y = value, color = stat)) + 
   theme_bw()
 
-#thoma's plot
+#thomas' plot
 ggplot(data = specerror[specerror$smpl %in% seq(10) & specerror$variable == "r_squared",], 
        aes(x = response, y = value, color = stat)) + 
   geom_boxplot() + 
@@ -26,8 +30,6 @@ ggplot(data = specerror[specerror$smpl %in% seq(10) & specerror$variable == "r_s
   geom_point(data = specerror[specerror$smpl == "Rsum" & specerror$variable == "r_squared",],
              aes(x = response, y = value, color = "R2 over all")) + 
   theme_bw()
-
-
 
 # with all exploratories
 train_pls_ffs<-readRDS("results/train_pls_9CV_3resp_7_pred.rds") 
@@ -46,3 +48,146 @@ ggplot(data = train_pls_ffs[train_pls_ffs$response %in% mr_all,],
   scale_fill_manual(values=cbPalette) +
   scale_colour_manual(values=cbPalette) +
   theme_bw()
+
+
+#my idea of visualisation of the Rmean values and the resamples
+stats<-errorspls_1LOO_SPEC
+#change factors to characters
+for(i in names(stats)[1:4]){
+  stats[[i]]<-as.character(stats[[i]])
+}
+# for Rsq
+# Rsq_pls<-ggplot(data= stats[stats$smpl %in% seq(10) & stats$variable=="r_squared" ,], 
+#        aes( x= factor(be) ,y=value, color=stat,  fill=stat),shape=21,color = "black", stroke=2)+
+#       geom_boxplot()+
+#   geom_point(data=stats[ stats$smpl == "Rmean" & stats$variable =="r_squared",],
+#              aes(x= factor(be), y=value,  color="Rmean"),shape=21,color = "black", stroke=2)+
+#   labs(title = "R squared Regression PLS after 9fold CV  ", 
+#               x = stats$response, y = "R squared", fill = "Mean of R² of resamples")+ 
+#    scale_colour_manual(values=cbPalette)+
+#   theme_bw()
+# 
+# # for RMSE
+# Rmse<-ggplot(data= stats[stats$smpl %in% seq(10) & stats$variable=="rmse" ,], 
+#        aes( x= factor(be) ,y=value, color=stat,  fill=stat),shape=21,color = "black", stroke=2)+
+#   geom_boxplot()+
+#   geom_point(data=stats[ stats$smpl == "Rmean" & stats$variable =="rmse",],
+#              aes(x= factor(be), y=value,  color="Rmean"),shape=21,color = "black", stroke=2)+
+#   labs(title = "RMSE Regression PLS after 9fold CV  ", 
+#        x = stats$response, y = "RMSE", fill = "Mean of RMSE of resamples")+ 
+#   scale_colour_manual(values=cbPalette)+
+#   theme_bw()
+# 
+# multiplot(Rsq,Rmse)
+
+
+# Rsq. bastelküche für mehrere responses 
+Rsq_glm<-ggplot(data= stats[stats$smpl %in% seq(10) & stats$variable=="r_squared"& stats$response %in% mr_all ,], 
+            aes( x= response ,y=value, color=stat,  fill=be),shape=21,color = "black", stroke=5)+
+            scale_color_hue(l=2, c=55)+ #l=lightness, c=intensity of color
+  # geom_boxplot(lwd=1)+
+  #stat_boxplot(geom = "errorbar",  width=0.1, size=1) +
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  geom_boxplot(lwd=0.7)+
+  geom_point(data=stats[ stats$smpl == "Rmean" & stats$variable =="r_squared"&
+                           stats$response %in% mr_all &stats$stat=="test",],
+             aes(x= response, y=value, color="test"),shape=22,color = "black", stroke=1, size=2)+
+  labs(title = "R squared Regression GLM after 9fold CV  ", 
+       x = stats$response, y = "R squared", fill = "Mean of R² of resamples")+ 
+  #guides(color=FALSE) + 
+  #scale_fill_manual(values=c("#FFCC99","#CCFFFF","#CCCCFF"))+ #"#FFCC99","#CC99CC","#99FFCC" "#6666CC","#FFCC99","#66CC99"
+  scale_fill_manual(values=wes_palette(n=3,name="Moonrise1"))+
+  #scale_fill_hue(l=100)
+  theme_bw()
+
+mr_all="LUI_glb"
+# RMSE bastelküche mehrere responses
+GAM_rmse<-ggplot(data= stats[stats$smpl %in% seq(10) & stats$variable=="rmse"& stats$response %in% mr_all ,], 
+       aes( x= be ,y=value, color=stat["train"],  fill=be),shape=21,color = "black", stroke=5)+
+  scale_color_hue(l=2, c=55)+ #l=lightness, c=intensity of color
+  # geom_boxplot(lwd=1)+
+  #stat_boxplot(geom = "errorbar",  width=0.1, size=1) +
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  geom_boxplot(lwd=0.7)+
+  geom_point(data=stats[ stats$smpl == "Rmean" & stats$variable =="rmse"&
+                           stats$response %in% mr_all &stats$stat=="test",],
+             aes(x= be, y=value, color="test"),shape=22,color = "black", stroke=1, size=2)+
+  labs(title = "RMSE Regression GAM after 9fold CV  ", 
+       x = "Exploratory", y = "RMSE", fill = "Mean of R² of resamples")+ 
+  #guides(color=FALSE) + 
+  #scale_fill_manual(values=c("#FFCC99","#CCFFFF","#CCCCFF"))+ #"#FFCC99","#CC99CC","#99FFCC" "#6666CC","#FFCC99","#66CC99"
+  scale_fill_manual(values=wes_palette(n=3,name="Moonrise1"))+
+  #scale_fill_hue(l=100)
+  theme_bw()+
+facet_grid(response ~ ., scales = "free")
+
+multiplot(GAM_rmse, pls_rmse, rf_rmse)
+
+rf<-readRDS(paste0(path_stats,"allnewstats_RF_9CV_allRESP.rds"))
+rf_rmse<-ggplot(data= rf[rf$smpl %in% seq(10) & rf$variable=="rmse"& rf$response %in% mr_all ,], 
+                 aes( x= response ,y=value, color=stat["train"],  fill=be),shape=21,color = "black", stroke=5)+
+  scale_color_hue(l=2, c=55)+ #l=lightness, c=intensity of color
+  # geom_boxplot(lwd=1)+
+  #stat_boxplot(geom = "errorbar",  width=0.1, size=1) +
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  geom_boxplot(lwd=0.7)+
+  geom_point(data=rf[ rf$smpl == "Rmean" & rf$variable =="rmse"&
+                           rf$response %in% mr_all &rf$stat=="test",],
+             aes(x= response, y=value, color="test"),shape=22,color = "black", stroke=1, size=2)+
+  labs(title = "RMSE Regression RF after 9fold CV  ", 
+       x = "Exploratory", y = "RMSE", fill = "Mean of R² of resamples")+ 
+  #coord_cartesian(xlim=NULL, ylim=c(m, max_xy))+
+  #guides(color=FALSE) + 
+  #scale_fill_manual(values=c("#FFCC99","#CCFFFF","#CCCCFF"))+ #"#FFCC99","#CC99CC","#99FFCC" "#6666CC","#FFCC99","#66CC99"
+  scale_fill_manual(values=c("#660066","#FFCC33","#CC6600"))+ #wes_palette(n=3,name="Moonrise1")
+  #scale_y_continuous( expand=waiver())+
+  #scale_fill_hue(l=100)
+  #theme_bw()+
+  #facet_grid(response ~ ., scales = "free")
+  facet_wrap(~response  , scales = "free")
+
+rf_range<-ggplot_build(rf_rmse)$layout$panel_ranges[[1]]$y.range
+
+# plots for pls
+pls<-readRDS(paste0(path_stats,"allnewstats_PLS_9CV_allRESP.rds"))
+Rsq_pls<-ggplot(data= pls[pls$smpl %in% seq(10) & pls$variable=="r_squared"& pls$response %in% mr_all ,], 
+                aes( x= response ,y=value, color=stat,  fill=be),shape=21,color = "black", stroke=5)+
+  scale_color_hue(l=2, c=55)+ #l=lightness, c=intensity of color
+  # geom_boxplot(lwd=1)+
+  #stat_boxplot(geom = "errorbar",  width=0.1, size=1) +
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  geom_boxplot(lwd=0.7)+
+  geom_point(data=pls[ pls$smpl == "Rmean" & pls$variable =="r_squared"&
+                           pls$response %in% mr_all &pls$stat=="test",],
+             aes(x= response, y=value, color="test"),shape=22,color = "black", stroke=1, size=2)+
+  labs(title = "R squared Regression pls after 9fold CV  ", 
+       x = pls$response, y = "R squared", fill = "Mean of R² of resamples")+ 
+  #guides(color=FALSE) + 
+  #scale_fill_manual(values=c("#FFCC99","#CCFFFF","#CCCCFF"))+ #"#FFCC99","#CC99CC","#99FFCC" "#6666CC","#FFCC99","#66CC99"
+  scale_fill_manual(values=wes_palette(n=3,name="Moonrise1"))+
+  #scale_fill_hue(l=100)
+  theme_bw()
+multiplot(Rsq_glm,Rsq_pls)
+
+mr_all="LUI_glb"
+# RMSE bastelküche mehrere responses
+pls_rmse<-ggplot(data= pls[pls$smpl %in% seq(10) & pls$variable=="rmse"& pls$response %in% mr_all ,], 
+                 aes( x= be ,y=value, color=stat["train"],  fill=be),shape=21,color = "black", stroke=5)+
+  scale_color_hue(l=2, c=55)+ #l=lightness, c=intensity of color
+  # geom_boxplot(lwd=1)+
+  #stat_boxplot(geom = "errorbar",  width=0.1, size=1) +
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+  geom_boxplot(lwd=0.7)+
+  geom_point(data=pls[ pls$smpl == "Rmean" & pls$variable =="rmse"&
+                           pls$response %in% mr_all &pls$stat=="test",],
+             aes(x= be, y=value, color="test"),shape=22,color = "black", stroke=1, size=2)+
+  labs(title = "RMSE Regression pls after 9fold CV  ", 
+       x = "Exploratory", y = "RMSE", fill = "Mean of R² of resamples")+ 
+  #guides(color=FALSE) + 
+  #scale_fill_manual(values=c("#FFCC99","#CCFFFF","#CCCCFF"))+ #"#FFCC99","#CC99CC","#99FFCC" "#6666CC","#FFCC99","#66CC99"
+  scale_fill_manual(values=wes_palette(n=3,name="Moonrise1"))+
+  #scale_fill_hue(l=100)
+  theme_bw()+
+  facet_grid(response ~ ., scales = "free")
+
+multiplot(GAM_rmse, pls_rmse)
