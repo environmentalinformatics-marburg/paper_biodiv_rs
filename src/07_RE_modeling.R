2175bccfcb1281552938b3cf1e5563769b2a53a1
Usel <ul.selig@gmail.com>
2019-02-26 11:16:25 +0100

    mutate responses for modeling in respect for NA for values

--- src/07_RE_modeling.R
+++ src/07_RE_modeling.R
@@ -4,29 +4,16 @@ library(doParallel)
 library(gpm)
 library(foreach)
 
-model_1a<-readRDS(paste0(path_rdata,"gpm_models_paper/RE_Model_1a.rds"))
+mods<-list.files(paste0(path_rdata,"gpm_models_paper/"), full.names=TRUE,pattern=glob2rx("*Model*"))
+#set variable for the specific mod you want the statistic
+x<-3
+mod_model<-readRDS(mods[x])
 
-cl<-makeCluster(3)
-registerDoParallel(cl)
-# 
-# r<- foreach(be=1:length(model_1a)) %dopar% {
-#   act_gpm <- model_1a[[be]]
-#   act_gpm <- trainModel(x = act_gpm_selected,
-#                         n_var = NULL,             
-#                         mthd = "pls", #rf, gam, glmboost
-#                         mode = "ffs",
-#                         seed_nbr = 11, 
-#                         cv_nbr = 5,
-#                         var_selection = "indv",
-#                         response_nbr = 1,
-#                         filepath_tmp = path_temp)
-#   model_1a[[be]] <- act_gpm
-# }
-# stopCluster(cl)
-
-veg_re_g_gpm_indv<- model_1a
-for(be in names(veg_re_g_gpm_indv)){
-  act_gpm_selected <- veg_re_g_gpm_indv[[be]]
+for(be in names(veg_model)){
+  cl <- makeCluster(3)
+  registerDoParallel(cl)
+  
+  act_gpm_selected <- veg_model[[be]]
   act_gpm_selected <- trainModel(x = act_gpm_selected,
                                  n_var = NULL,             
                                  mthd = "pls", #rf, gam, glmboost
@@ -34,8 +21,18 @@ for(be in names(veg_re_g_gpm_indv)){
                                  seed_nbr = 11, 
                                  cv_nbr = 5,
                                  var_selection = "indv",
+                                 response_nbr= c(1:3,5,12,14),
                                  filepath_tmp = path_temp)
-  veg_re_g_gpm_indv[[be]] <- act_gpm_selected
+  veg_model[[be]] <- act_gpm_selected
+  
+  stopCluster(cl)
 }
 
-saveRDS(veg_re_g_gpm_indv, "9CV_pls_model_7pred.rds")
+Model_0_RE_pls_ffs
+Model_3_RELUI_pls_ffs
+Model_4_CLIMALUI_pls_ffs
+Model_5_RECLIMALUI_pls_ffs
+
+
+saveRDS(veg_model, "Model_0_RE_pls_ffs.rds")
+
--- src/07_compile gpm_models.R
+++ src/07_compile gpm_models.R
@@ -3,14 +3,35 @@ source("C:/Users/uselig/Documents/GitHub/project_biodiv_rs/src/00_paths_librarie
 #read base gpm object
 veg_re_g_gpm_indv<-readRDS(paste0(path_rdata, "gpm_models_paper/GPM-base-object.rds"))
 
-######################################################################################
-#################################################
-############################
-# SPECIFY the model
-###### RESPONSE-FINAL BRINGT NICHTS! WIRD IN GPM TRAINING ANSCHEINEND NICHT BERÜCKSICHTIGT (Stand januar 2019)
-
 #________________________________________________________________________________________________
-#__________________________________________
+############################
+# SPECIFY the model RESPONSE
+############################__________________________________________
+###### NOTE: RESPONSE-FINAL BRINGT NICHTS! WIRD IN GPM TRAINING ANSCHEINEND NICHT BERÜCKSICHTIGT (Stand januar 2019),
+####### geht dann nur im trainModel Aufruf mit z.B. response_nbr= c(1:3,5,12,14)
+
+# in manchen plots sind in der response NA Werte, die Modelle können dann nicht rechnen, deswegen nehmen wir sie erst jetzt an dieser Stelle raus
+# und nicht bevor wir die TrainTest Selektion festlegen, da sonst die ganze Zeile herausgelöscht werden muss,wo alle anderen responses auch drin sind
+
+# remove HEG09 and HEG44 only in response traintest biomass
+for (train in seq(length(veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]]))){
+  veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]][[train]]$training$SAMPLES<- veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]][[train]]$training$SAMPLES[
+    veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]][[train]]$training$SAMPLES != 9] #lösche HEG09 in biomass (14)
+  veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]][[train]]$training$SAMPLES<- veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]][[train]]$training$SAMPLES[
+    veg_re_g_gpm_indv$HEG@meta$input$TRAIN_TEST[[14]][[train]]$training$SAMPLES != 44] #lösche HEG44 in biomass (14)
+  
+}
+
+# remove AEG09 only from response bare soil
+for (train in seq(length(veg_re_g_gpm_indv$AEG@meta$input$TRAIN_TEST[[12]]))){
+  veg_re_g_gpm_indv$AEG@meta$input$TRAIN_TEST[[12]][[train]]$training$SAMPLES<- veg_re_g_gpm_indv$AEG@meta$input$TRAIN_TEST[[12]][[train]]$training$SAMPLES[
+    veg_re_g_gpm_indv$AEG@meta$input$TRAIN_TEST[[12]][[train]]$training$SAMPLES != 9] #lösche AEG09 in baresoil (12)
+  
+}
+#________________________________________________________________________________________________
+############################
+# SPECIFY the model PREDICTORS
+############################__________________________________________
 
 ## MODEL 1: RE predictors "model 1": predictors are the same in all BE
   mod1<- veg_re_g_gpm_indv
@@ -25,10 +46,9 @@ veg_re_g_gpm_indv<-readRDS(paste0(path_rdata, "gpm_models_paper/GPM-base-object.
   
   predictors_common <- predictors_common[order(predictors_common)]
   #save the common predictors in the final variable
-  mod1[[1]]@meta$input$PREDICTOR_FINAL <- predictors_common
-  mod1[[2]]@meta$input$PREDICTOR_FINAL <- predictors_common
-  mod1[[3]]@meta$input$PREDICTOR_FINAL <- predictors_common
-
+  for(be in names(mod1)){
+    mod1[[be]]@meta$input$PREDICTOR_FINAL<- predictors_common
+  }
 # save the model
 saveRDS(mod1, paste0(path_rdata, "gpm_models_paper/RE_Model_1.rds"))
 #________________________________________________________________________________________________
@@ -49,11 +69,11 @@ saveRDS(mod0, paste0(path_rdata, "gpm_models_paper/RE_Model_0.rds"))
 #__________________________________________
 
 # SPECIFY the model
-# Model 2- use ONLY LUI components as final predictors
+# Model 2- use ONLY LUI components as final predictors, but we only use regional components ("LUI_reg" etc.)
 mod2<-veg_re_g_gpm_indv
 ## choose FINAL predictors for LUI only model (Model2)- all predictors are chosen for all BE
 for(be in names(mod2)){
-  mod2[[be]]@meta$input$PREDICTOR_FINAL<- mod2[[be]]@meta$input$PREDICTOR[1:8]
+  mod2[[be]]@meta$input$PREDICTOR_FINAL<- mod2[[be]]@meta$input$PREDICTOR[5:8]
 }
 
 # save the model
